09e13e8fd8cafafbe0b8fc09d95aa1735837cd61
PradhyumnaVA <137704414+PradhyumnaVA@users.noreply.github.com>
2023-08-28 14:55:38 +0530

    Update test_case_01.py

--- pyinterface/test_case_01.py
+++ pyinterface/test_case_01.py
@@ -10,13 +10,10 @@ while(True):
     b = ser.readline()         # read a byte string
     string_n = b.decode()  # decode byte string into Unicode
     string = string_n.rstrip() # remove \n and \r
-    #flt = string        # convert string to float
-    #print(flt)
-    #data.append(flt)           # add to the end of data list
     time.sleep(0.1)            # wait (sleep) 0.1 seconds
     if (string == "yes"):
         pyautogui.press('right')
-#ser.close()
+
 
 
 
